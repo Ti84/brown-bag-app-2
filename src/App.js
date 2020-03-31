f450cbdf783b852252e923fc2cea46f8694a092b
Shane4666 <sc897@nau.edu>
2020-03-31 16:29:37 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './components/layout/Header';
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { IoIosPin } from "react-icons/io";
+
+const Header = () => (
+  <header className="header">
+    <nav>
+      <div className="logo">
+        <IoIosPin />
+        <h1>Shane's Car Lot</h1>
+      </div>
+    </nav>
+  </header>
+);
+
+export default Header;
